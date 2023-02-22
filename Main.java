eb5962661224b4e6e1fd389e37cf8c3408472acd
Ziyoda Muslimova <121525223+Ziyoda1988@users.noreply.github.com>
2023-02-22 18:18:26 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+class Main {
+  public static void main(String[] args) {
+    //Test your methods
+
+    char[] arr = {'h', 'e', 'l', 'l', 'o'};
+     subArray1(arr,4);
+    subArray1(arr, 1 ,4);
+    
+
+  }
+    //Create your methods here
+  public static void subArray1 (char arr[], int begin){
+    char newArr [] = new char [arr.length - begin];
+    for (int i = 0; i<arr.length - begin; i++){
+      newArr[i] = arr[i+begin];
+        }
+    System.out.println(Arrays.toString(newArr));
+  }
+  public static void subArray1 (char arr[], int begin, int end){
+    char newArr[]=new char[end - begin];
+    for (int i = 0; i < end - begin; i++){
+      newArr[i] = arr[i+begin];
+      }
+    System.out.println(Arrays.toString(newArr));
+    
+  }
+}
